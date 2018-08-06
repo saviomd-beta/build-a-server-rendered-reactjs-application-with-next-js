a49bb4c09e08914daf0857c15a70415ca318b72f
Sávio Mendes <saviomd@gmail.com>
2018-08-06 12:48:51 -0300

    Lesson 4

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
+import Header from '../components/header';
+import withMui from '../shared/MUI/withMUI';
+
 const Index = ({ title = 'Hello from next.js' }) =>
   <div>
+    <Header />
     <h2>{title}</h2>
   </div>
 
-export default Index;
+export default withMui(Index);
