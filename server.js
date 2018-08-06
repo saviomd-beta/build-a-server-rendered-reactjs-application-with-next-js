56584dfc35b789c3d17a7deaa5371b5cd2c499f2
Sávio Mendes <saviomd@gmail.com>
2018-08-06 16:18:05 -0300

    Lesson 7

--- server.js
+++ server.js
@@ -12,10 +12,19 @@ app.prepare().then(() => {
 
   server.get('/', (req, res) => res.redirect(301, '/blog'));
 
+  server.get('/blog/:id', (req, res) => {
+    return app.render(req, res, '/post', Object.assign({id: req.params.id}, req.query));
+  });
+
+  server.get('/post', (req, res) => {
+    if (req.query.id) return res.redirect(`/blog/${req.query.id}`);
+    res.redirect(301, '/blog');
+  });
+
   server.get('/*', (req, res) => handle(req, res));
 
   server.listen(port, err => {
     if (err) throw err;
-    console.log('> Read on http://localhost:${port}');
+    console.log(`> Read on http://localhost:${port}`);
   });
 });
